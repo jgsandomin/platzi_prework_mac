bd7e0f264646c077872001fd54637f15cdc9cd6e
JG Sandoval <88694914+jgsandomin@users.noreply.github.com>
2021-09-18 21:08:50 -0500

    using variables

--- Python/functions.py
+++ Python/functions.py
@@ -1,15 +1,22 @@
-def conversation(message):
-    print("Hello")
-    print("How are you?")
-    print(message)
-    print("Bye!")
+# def conversation(message):
+#     print("Hello")
+#     print("How are you?")
+#     print(message)
+#     print("Bye!")
 
-option = int(input("Choice an option (1, 2, 3): "))
-if option == 1:
-    conversation("You chose the option 1")
-elif option == 2:
-    conversation("You chose the option 2")
-elif option == 3:
-    conversation("You chose the option 3")
-else:
-    print("You should select a valid option")
+# option = int(input("Choice an option (1, 2, 3): "))
+# if option == 1:
+#     conversation("You chose the option 1")
+# elif option == 2:
+#     conversation("You chose the option 2")
+# elif option == 3:
+#     conversation("You chose the option 3")
+# else:
+#     print("You should select a valid option")
+
+def sum (a,b):
+    print("Two numbers are added")
+    result = a + b
+    return result
+summation = sum (1,4)
+print(summation)
